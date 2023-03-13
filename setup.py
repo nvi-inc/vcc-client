d11afe3a294514ea0d0554a024d068da3ea3f15f
mario-berube <98411521+mario-berube@users.noreply.github.com>
2023-03-13 08:52:34 -0400

    Add dist with whl files

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup
 setup(
     name='vcc',
     packages=['vcc', 'vcc.ns'],
-    description='VCC Schedule uploader for Operations Center client',
+    description='Client software for VLBI Operations Center (VCC)',
     version='1.2.0',
-    url='http://github.com/example/linode_example',
+    url='http://github.com/',
     author='Mario',
     author_email='mario.berube@nviinc.com',
     keywords=['vlbi', 'vcc'],
     install_requires=['requests', 'sshtunnel', 'pika', 'toml', 'psutil', 'pexpect',
                       'pycryptodome', 'pyjwt', 'urllib3', 'PyQt5', 'tabulate'],
     include_package_data=True,
-    # package_data={'': ['data/sv.bin']},
     entry_points={
         'console_scripts': [
             'vcc=vcc.__main__:main',
